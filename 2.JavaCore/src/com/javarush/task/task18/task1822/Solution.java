714c1f2af364b9e799606df5fb6b1b033f4fe5b9
waukens <my4all@yandex.ru>
2017-04-09 16:48:28 +0300

    task 1882 closed

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -1,19 +1,26 @@
 package com.javarush.task.task18.task1822;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /* 
 Поиск данных внутри файла
 */
 
 public class Solution {
+
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
-        FileInputStream fi = new FileInputStream(fileName);
-
+        BufferedReader fi = new BufferedReader(new FileReader(fileName));
+        String s = "";
+        String id = args[0];
+        while ((s = fi.readLine()) != null) {
+            if (s.split(" ")[0].equals(id)) {
+                System.out.println(s);
+                break;
+            }
+        }
+        fi.close();
+        reader.close();
     }
 }
