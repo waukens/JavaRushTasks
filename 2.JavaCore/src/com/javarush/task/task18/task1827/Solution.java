87bb9b29880345f122020bec21b123ae9fecd094
waukens <my4all@yandex.ru>
2017-04-13 22:46:37 +0300

    task 1827 add

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -3,8 +3,20 @@ package com.javarush.task.task18.task1827;
 /* 
 Прайсы
 */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        String productName = args[1];
+        String price = args[2];
+        String quatity = args[3];
+        String id = "";
+
+
     }
 }
