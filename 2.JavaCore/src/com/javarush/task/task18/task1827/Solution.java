c8d3c5e865fabd5bf7ca42ed19b33b38b586c755
waukens <my4all@yandex.ru>
2017-04-22 22:52:38 +0300

    add task 1823

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -5,17 +5,24 @@ package com.javarush.task.task18.task1827;
 */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
+import java.io.RandomAccessFile;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
-        String productName = args[1];
-        String price = args[2];
-        String quatity = args[3];
-        String id = "";
+//        String productName = args[1];
+//        String price = args[2];
+//        String quatity = args[3];
+        int id = 0;
+        RandomAccessFile ra = new RandomAccessFile(fileName, "rw");
+        if (ra.length() >= 80) {
+            ra.seek(ra.length() - 80);
+            byte[] buffer = new byte[8];
+            id = ra.read(buffer, 0, 8);
+        }
+        System.out.println(id);
+
 
 
     }
