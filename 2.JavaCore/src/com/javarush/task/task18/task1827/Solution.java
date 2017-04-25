c8a58f3564ad22e785b60b4706ff85a8d2604f36
waukens <my4all@yandex.ru>
2017-04-25 14:51:32 +0300

    task 1903 add

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -3,27 +3,50 @@ package com.javarush.task.task18.task1827;
 /* 
 Прайсы
 */
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
-//        String productName = args[1];
-//        String price = args[2];
-//        String quatity = args[3];
-        int id = 0;
-        RandomAccessFile ra = new RandomAccessFile(fileName, "rw");
-        if (ra.length() >= 80) {
-            ra.seek(ra.length() - 80);
-            byte[] buffer = new byte[8];
-            id = ra.read(buffer, 0, 8);
+        ArrayList<Integer> indexs = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>();
+        if (args[0].equals("-c")) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < args.length - 2; i++) {
+                sb.append(args[i]);
+                sb.append(" ");
+            }
+            String productName = sb.toString();
+            String price = args[args.length - 2];
+            String quatity = args[args.length - 1];
+            BufferedReader fBR = new BufferedReader(new FileReader(fileName));
+            Pattern p = Pattern.compile("[0-9]+");
+            while (fBR.ready()) {
+                String line = fBR.readLine();
+                lines.add(line);
+                System.out.println(line);
+//                Matcher mstch = p.matcher(line);
+//                mstch.find();
+//                indexs.add(Integer.parseInt(mstch.group(0)));
+            }
+            for (int i : indexs) {
+                System.out.println(i);
+            }
+
+//            String lastLine = String.format("%-8d%-30.30s%-8.8s%-4.4s", nextInd, productName, price, quatity);
+//            FileOutputStream fo = new FileOutputStream(fileName);
+//            for (int i = 0; i < data.size(); i++) {
+//                fo.write(data.get(i));
+//            }
+//            fo.write(13);
+//            fo.write(10);
+//            fo.write(lastLine.getBytes());
+//            fo.write(13);
+//            fo.write(10);
+//            fo.close();
         }
-        System.out.println(id);
-
-
-
     }
 }
--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -10,18 +10,46 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
 
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+
     public static void main(String[] args) {
 
     }
 
-    public static class IncomeDataAdapter implements IncomeData, Customer {
+    public static class IncomeDataAdapter implements Contact, Customer {
 
         private IncomeData data;
 
-        static {
-            countries.put("UA", "Ukraine");
-            countries.put("RU", "Russia");
-            countries.put("CA", "Canada");
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getName() {
+            return this.data.getContactLastName() + ", " + this.data.getContactFirstName();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(this.data.getCountryCode());
+        }
+
+        @Override
+        public String getCompanyName() {
+            return this.data.getCompany();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String tel = String.format("%010d", this.data.getPhoneNumber());
+            return String.format("+%d(%s)%s-%s-%s", this.data.getCountryPhoneCode(),
+                    tel.substring(0, 3), tel.substring(3, 6), tel.substring(6, 8),
+                    tel.substring(8, 10));
         }
     }
 
