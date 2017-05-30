dc86bbde6b1ed6e47c35234be4d10c3dae7fc9e0
waukens <my4all@yandex.ru>
2017-05-30 09:11:24 +0300

    task update

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -11,22 +11,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
         reader.close();
         FileReader fReader = new FileReader(fileName);
         StringBuffer sb = new StringBuffer();
         while (fReader.ready()) {
-            sb.append((char)fReader.read());
+            sb.append((char) fReader.read());
         }
         String line = sb.toString();
+        System.out.println(line);
         String tag = args[0];
-        String patLine = "(<" + tag + ".*>).*(<" + tag + ".*>)?.*(</" + tag + ">)?.*(</" + tag + ">)";
+        String patLine = "(<" + tag + ">)(<"+tag+">)?(\r\n)?(</" + tag + ">)(.*(</" + tag + ">))?";
+        System.out.println(patLine);
         Pattern p = Pattern.compile(patLine);
         Matcher match = p.matcher(line);
+        System.out.println("Answer");
         while (match.find()) {
             System.out.println(match.group());
+            System.out.println("group size" + match.group().length());
         }
     }
 }
