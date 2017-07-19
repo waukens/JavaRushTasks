ac8ba171ddb691500ba15de3ee5f9c9c81545055
waukens <my4all@yandex.ru>
2017-07-19 18:30:24 +0300

    Task 2304 completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2304/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task23.task2304;
+
+import java.util.List;
+import java.util.Map;
+
+/* 
+Inner 3
+*/
+public class Solution {
+
+    private List<Task> tasks;
+    private List<String> names;
+
+    private DbDataProvider taskDataProvider = new TaskDataProvider();
+    private DbDataProvider nameDataProvider = new NameDataProvider();
+
+    private class TaskDataProvider implements DbDataProvider<Task> {
+        @Override
+        public void refreshAllData(Map criteria) {
+            DbMock dbMock = new DbMock();
+            Solution.this.tasks = DbMock.getFakeTasks(criteria);
+        }
+    }
+
+    private class NameDataProvider implements DbDataProvider<String> {
+        @Override
+        public void refreshAllData(Map criteria) {
+            DbMock dbMock = new DbMock();
+            Solution.this.names = DbMock.getFakeNames(criteria);
+        }
+    }
+
+    public void refresh() {
+        Map taskCriteria = ViewMock.getFakeTasksCriteria();
+        taskDataProvider.refreshAllData(taskCriteria);
+
+        Map nameCriteria = ViewMock.getFakeNamesCriteria();
+        nameDataProvider.refreshAllData(nameCriteria);
+    }
+
+    private interface DbDataProvider<T> {
+        void refreshAllData(Map criteria);
+    }
+
+    class Task {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
