79602664ded7fa8155f950097af1ca308df25bf1
waukens <my4all@yandex.ru>
2017-07-24 17:30:28 +0300

    Task 2313 part10 completed

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -34,4 +34,20 @@ public class Canvas {
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
     }
+
+    public void setPoint(double x, double y, char c) {
+        int roundX = (int)Math.round(x);
+        int roundY = (int)Math.round(y);
+        if ((roundX >= 0 && roundX < matrix[0].length) && (roundY >= 0 && roundY < matrix.length)) {
+            matrix[roundY][roundX] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = (int)Math.round(y); i < matrix.length; i++) {
+            for (int j = (int)Math.round(x); j < matrix[0].length; j++) {
+                setPoint(i, j, c);
+            }
+        }
+    }
 }
