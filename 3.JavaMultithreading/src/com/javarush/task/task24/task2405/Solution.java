bf607040b4959210c23961fafd37f1b8387e65e4
waukens <my4all@yandex.ru>
2017-08-01 09:01:55 +0300

    Task 2405 completed

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2405/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2405/Solution.java
@@ -9,12 +9,37 @@ public class Solution implements Action {
     private int param;
 
     private Action solutionAction = new Action() {
+        FirstClass firstClass;
+        SecondClass secondClass;
         //!!!!! Changes can be here
         //!!!!! Изменения могут быть тут
 
         public void someAction() {
-            //!!!!! All changes have to be here
-            //!!!!! Все изменения должны быть только тут
+            if (param > 0) {
+                while (param > 0) {
+                    System.out.println(param--);
+                }
+                new FirstClass() {
+                    @Override
+                    public Action getDependantAction() {
+                        super.someAction();
+                        return new Action() {
+                            @Override
+                            public void someAction() {
+                            }
+                        };
+                    }
+                }.getDependantAction();
+                //!!!!! All changes have to be here
+                //!!!!! Все изменения должны быть только тут
+            }
+            new SecondClass() {
+                @Override
+                public void someAction() {
+                    super.someAction();
+                }
+            }.someAction();
+            System.out.println(secondClass.SPECIFIC_ACTION_FOR_ANONYMOUS_SECOND_CLASS_PARAM.replaceFirst("\\n", "") + param);
         }
     };
 
