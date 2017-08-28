5c26dd1197fa8f5dea342e2b8fdee88c80073eff
waukens <my4all@yandex.ru>
2017-08-28 17:51:16 +0300

    Task 2909 part 9 completed

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -10,22 +10,18 @@ public class Human implements Alive {
     protected int age;
     protected String name;
 
-    protected int[] size;
+    protected Size size;
 
     private List<Human> children = new ArrayList<>();
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
     }
 
-    public int getBloodGroup() {
-        return bloodGroup;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
     public Human(String name, int age) {
@@ -60,7 +56,7 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public void addChild(Human human) {
@@ -84,5 +80,15 @@ public class Human implements Alive {
 
     public void printData() {
         System.out.println(getPosition() + ": " + name);
+    }
+
+    public class Size {
+        public int height;
+        public int weight;
+
+        public Size(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
     }
 }
