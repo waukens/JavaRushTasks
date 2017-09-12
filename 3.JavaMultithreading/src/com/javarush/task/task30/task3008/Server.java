071b4fcd33d5ed7e45d652e8fe8838392916db73
waukens <my4all@yandex.ru>
2017-09-12 21:05:36 +0300

    Tasks 3008 part 6 completed

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
     public static void main(String[] args) {
+        int socketPort = ConsoleHelper.readInt();
+        try (ServerSocket ss = new ServerSocket(socketPort); ) {
+        ConsoleHelper.writeMessage("Сервер запущен.");
+            while (true) {
+                    Socket socket = ss.accept();
+                    new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Oops!");
+        } finally {
+
+        }
 
     }
 }
