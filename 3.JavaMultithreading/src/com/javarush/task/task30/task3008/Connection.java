570506823048800448af608251cea2caba777490
waukens <my4all@yandex.ru>
2017-09-12 09:01:24 +0300

    Tasks 3008 part 4 added

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,11 +1,12 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Connection {
+public class Connection implements Closeable {
     private final Socket socket;
     private final ObjectOutputStream out;
     private final ObjectInputStream in;
@@ -14,5 +15,27 @@ public class Connection {
         this.socket = socket;
         this.out = new ObjectOutputStream(socket.getOutputStream());
         this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+            out.flush();
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (in) {
+            return (Message) in.readObject();
+        }
+    }
+
+    public  SocketAddress getRemoteSocketAddress(), в
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+        in.close();
+        socket.close();
     }
 }
