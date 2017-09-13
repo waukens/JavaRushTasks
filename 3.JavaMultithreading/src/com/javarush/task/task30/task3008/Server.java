a3e7c7b22f6e837fce16142200fd3183975e965e
waukens <my4all@yandex.ru>
2017-09-14 01:29:55 +0300

    Tasks 3008 part 13 completed

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -29,7 +29,50 @@ public class Server {
                 }
             }
         }
+
+        private void sendListOfUsers(Connection connection, String userName) throws IOException {
+            for (String key: connectionMap.keySet()) {
+                if (!userName.equals(key)) {
+                    connection.send(new Message(MessageType.USER_ADDED, key));
+                }
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message replay = connection.receive();
+                if (replay.getType() == MessageType.TEXT) {
+                    sendBroadcastMessage(new Message(MessageType.TEXT,
+                            String.format("%s: %s", userName, replay.getData())));
+                } else {
+                    ConsoleHelper.writeMessage("Wrong type of message.");
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            ConsoleHelper.writeMessage(String.format("Было установлено соединение с удаленным адресом %s",
+                    socket.getRemoteSocketAddress()));
+            try (Connection connection = new Connection(socket)) {
+                String clientName = serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, clientName));
+                sendListOfUsers(connection, clientName);
+                serverMainLoop(connection, clientName);
+                if (!clientName.equals(null)) {
+                    connectionMap.remove(clientName);
+                    sendBroadcastMessage(new Message(MessageType.USER_REMOVED, clientName));
+                }
+                System.out.println("Соединение с сервером закрыто.");
+            } catch (IOException e) {
+                System.out.println(e);
+            } catch (ClassNotFoundException t) {
+                System.out.println(t);
+            }
+        }
     }
+
 
     public static void sendBroadcastMessage(Message message) {
         for (String key : connectionMap.keySet()) {
