2354a9f80f329a4cc0a7531478a65c745b9b85ad
waukens <my4all@yandex.ru>
2017-09-14 08:54:25 +0300

    Tasks 3008 part 16 added

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,12 +1,111 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
-    
-    public class SocketThread extends Thread {
 
+    protected String getServerAddress() {
+        System.out.println("Введитье адрес сервера");
+        return ConsoleHelper.readString();
+
+    }
+
+    protected int getServerPort() {
+        System.out.println("Введитье порт сервера");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        System.out.println("Введитье имя пользователя");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            e.printStackTrace();
+            clientConnected = false;
+        }
+    }
+
+    public void run(){
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+            if (clientConnected == true) {
+                ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду ‘exit’.");
+
+            } else {
+                ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            }
+            while (clientConnected) {
+                String text = ConsoleHelper.readString();
+                if (text.equals("exit")) {
+                    break;
+                }
+                if (shouldSendTextFromConsole()) {
+                    sendTextMessage(text);
+                }
+
+            }
+        }
+
+    }
+
+    public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage(String.format("Участник с именем %s присоединился к чату.", userName));
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage(String.format("Участник с именем %s покинул к чат.", userName));
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            synchronized (Client.this) {
+                Client.this.clientConnected = clientConnected;
+                Client.this.notify();
+
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message replay = connection.receive();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
     }
 }
