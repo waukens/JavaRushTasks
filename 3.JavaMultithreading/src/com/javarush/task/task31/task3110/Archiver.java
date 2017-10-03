6ef8ad18dc2b6ffe6dd2ea6b15fc209a728f661e
waukens <my4all@yandex.ru>
2017-10-03 09:08:18 +0300

    Tasks 3110 part 1 completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter file/folder's path to zip:");
+        try {
+            ZipFileManager zFm = new ZipFileManager(Paths.get(reader.readLine()));
+            System.out.print("Enter path to archiver: ");
+            zFm.createZip(Paths.get(reader.readLine()));
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
