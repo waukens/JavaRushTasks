b993ae55806467877e55038b58cda399727f8caa
waukens <my4all@yandex.ru>
2017-10-04 08:34:04 +0300

    Tasks 3110 part 2 completed

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,6 +14,20 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (
+                ZipOutputStream zOs = new ZipOutputStream(Files.newOutputStream(zipFile));
+                InputStream iS = Files.newInputStream(source)
+        ) {
+            ZipEntry zE = new ZipEntry(source.getFileName().toString());
+            zOs.putNextEntry(zE);
+            while (iS.available() > 0) {
+                zOs.write(iS.read());
+            }
+            zOs.closeEntry();
+        }
+
+
+
 
     }
 }
