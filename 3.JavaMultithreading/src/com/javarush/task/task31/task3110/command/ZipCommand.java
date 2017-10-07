d945f2241a7359092da4008942c36dca8c686b56
waukens <my4all@yandex.ru>
2017-10-07 20:34:23 +0300

    tasks 3110 part 11 completed

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива: ");
+        String zipPath = ConsoleHelper.readString();
+        return new ZipFileManager(Paths.get(zipPath));
+    }
 }
