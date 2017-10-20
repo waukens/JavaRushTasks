eb465caeaa68cadb4dd5ace25722e06f53be2d32
waukens <my4all@yandex.ru>
2017-10-20 09:11:02 +0300

    Tasks 2802 completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2802/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task28.task2802;
+
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Пишем свою ThreadFactory
+*/
+public class Solution {
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+        static final AtomicInteger poolNumber = new AtomicInteger(1);
+        final AtomicInteger threadNumber = new AtomicInteger(1);
+        final ThreadGroup group;
+        final String namePrefix;
+
+        public AmigoThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+            namePrefix = String.format("%s-pool-%s-thread-", group.getName(),
+                    poolNumber.getAndIncrement());
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
+    }
+
+    public static void main(String[] args) {
+        class EmulateThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulateThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulateThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+}
