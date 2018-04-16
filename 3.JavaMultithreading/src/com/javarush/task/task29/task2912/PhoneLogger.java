568af8f855ee7b57b031dfe407ac82b99edf36d2
waukens <my4all@yandex.ru>
2018-04-16 10:18:01 +0300

    Task 2905, 2906, 2907, 2910, 2911, 2912, 2913 completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2912/PhoneLogger.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task29.task2912;
+
+public class PhoneLogger extends AbstractLogger {
+    public PhoneLogger(int level) {
+        super.level = level;
+    }
+
+    @Override
+    public void info(String message) {
+        System.out.println("Call to director: " + message);
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task29.task2913;
+
+import java.util.Random;
+
+/* 
+Замена рекурсии
+*/
+
+public class Solution {
+    private static int numberA;
+    private static int numberB;
+
+    public static String getAllNumbersBetween(int a, int b) {
+        StringBuffer sb = new StringBuffer();
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                sb.append(i + " ");
+            }
+        } else {
+            for (int i = a; i >= b; i--) {
+                sb.append(i + " ");
+            }
+        }
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        numberA = random.nextInt() % 1_000;
+        numberB = random.nextInt() % 10_000;
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
+    }
+}
