bd110b06c2f252aa3d234299db4f1870f5df03bf
waukens <my4all@yandex.ru>
2018-05-15 21:55:01 +0300

    Task 2712 part 5 completed

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
@@ -14,8 +14,9 @@ public class Cook extends Observable implements Observer{
 
     @Override
     public void update(Observable observable, Object arg) {
+        Order order = (Order) arg;
         System.out.println(arg);
-        System.out.println("Start cooking - " + arg);
+        System.out.println("Start cooking - " + arg + ", cooking time " + order.getTotalCookingTime() + "min");
         this.setChanged();
         this.notifyObservers(arg);
     }
