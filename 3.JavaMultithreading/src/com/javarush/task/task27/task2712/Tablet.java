8fa03afa14896e2cd885badbb90709f0f3c83cf3
waukens <my4all@yandex.ru>
2018-05-16 22:24:05 +0300

    Task 2712 part 8 completed

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.ad.AdvertisementManager;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
@@ -21,6 +22,8 @@ public class Tablet extends Observable {
             if (!order.isEmpty()) {
                 this.setChanged();
                 this.notifyObservers(order);
+                AdvertisementManager advertisementManager = new AdvertisementManager(order.getTotalCookingTime() * 60);
+                advertisementManager.processVideos();
             }
             return order;
         } catch (IOException e) {
